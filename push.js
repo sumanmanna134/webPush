d6628cd1d185e4267bccf3de97f922be4b66a85d
Suman Manna <manna.suman134@gmail.com>
2020-08-21 13:11:56 +0530

    fix

--- push.js
+++ push.js
@@ -5,5 +5,13 @@ let vapIdKeys = {
 }
 
 push.setVapidDetails('mailto: test@channelier.com',vapIdKeys.publicKey,vapIdKeys.privateKey)
-let sub = {}
+let sub = {
+    "endpoint":"https://fcm.googleapis.com/fcm/send/dLcLMG0MA-U:APA91bEkUwfJXcDiOS0cfO7Pa_9L8UrQcKxbgJ1twZThkBcXNirpaG5P7bLhwQnKx5jQ_SJMB1DKYVMFMJNSvq8QyXJF-sD88CqjrOmjJ7v-3r_RTIE7WPxU4udwkOPDevA0-ZDOvILJ",
+    "expirationTime":null,
+    "keys":{
+        "p256dh":
+        "BFJRKFozGJFZl2hCCTQfLE7IfBzPGA9GMOYoV8qMxUdJyK2i0Af_BZKh5BCp_QJsPoUND77DJJTYCUqElcR5JhQ",
+        "auth":"hb2b504qUc2njFNIgUCYDw"
+    }
+}
 push.sendNotification(sub,'test message')
--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
-self.addEventListener('push', ()=> {
-    self.registration.sendNotification('test_message', {});
-});
+self.addEventListener('push', function(e) {
+    var options = {
+      body: 'Channelier | Notification Testing!',
+      icon: 'https://channelier.com/image/data/logo.png',
+      vibrate: [100, 50, 100],
+      data: {
+        dateOfArrival: Date.now(),
+        primaryKey: '2'
+      },
+      actions: [
+        {
+            action: 'explore', title: 'Explore this new world',
+            icon: 'images/checkmark.png'
+        },
+        {   action: 'close', title: 'Close',
+            icon: 'images/xmark.png'
+        },
+      ]
+    };
+    e.waitUntil(
+      self.registration.showNotification('Welcome to Channelier!', options)
+    );
+  });
